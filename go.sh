8b97a2591ae37101f8d1c7b07eb592062a5d3f3c
AkihiroTanaka <tanaka-a@vrain.co.jp>
2023-02-17 05:23:27 +0000

    add AIcall

--- go.sh
+++ go.sh
@@ -4,15 +4,18 @@
 SCRIPT_DIR=$(cd &(dirname $0); pwd)
 # カレントディレクトリをスクリプトの位置に移動
 cd $SCRIPT_DIR
-
+#ログ出力フォルダを生成
+mkdir -p output_data
 
 #全CPUを100%で可動させる
 sudo stress-ng -c 0&
+# GPU高負荷実行　領域抽出繰り返し
+/usr/bin/python3 python/AI.py &
 
 #1[s]間隔で実行中のデータをログに吐き出す。
 sudo tegrastats --interval 1000 --logfile output_data/data_$(date +"%m%d%H%M").txt &
 
-sleep 1
+sleep 10
 #実行区間中にjtopを実行しモニタリングできるようにする。
 #スクリプト実行後jtopが起動できなくなることがあるため表示させない。
 #sudo jtop
@@ -23,11 +26,14 @@ read wait
 
 #ログ保存の終了
 sudo tegrastats --stop
+sleep 1
 
 
 #負荷試験終了
 sudo pkill stress-ng
+sudo pkill python3
 sleep 1
+
 
 #カレントディレクトリを元の位置に戻す
 cd -
